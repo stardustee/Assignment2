aba2edff5fa2e7be7e5ce2a2d63391dda928075a
sydney <sydneyhuang@rogers.com>
2020-10-23 14:13:32 -0400

    Titles

--- server/config/DB.js
+++ server/config/DB.js
@@ -1,6 +1,8 @@
 //module.exports.URL = "mongodb://localhost/user_collections";
 //module.exports.URL = "mongodb://localhost/buisness-contacts";
 
+
+
 module.exports.URL ="mongodb+srv://sydney:1ej0zBEikL4YBfMs@mongodbserver.sffhz.mongodb.net/buisness-contacts?retryWrites=true&w=majority";
 
 
